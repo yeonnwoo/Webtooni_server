e7308e3b8dc200c3a55ebd070532b9bb983e6cdb
Yeonwoo-Kim <yeonkim0726@gmail.com>
2021-07-27 18:59:42 +0900

    initial commit

---
+++ src/main/java/com/webtooni/webtooniverse/WebtooniverseApplication.java
@@ -0,0 +1,13 @@
+package com.webtooni.webtooniverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtooniverseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebtooniverseApplication.class, args);
+    }
+
+}
