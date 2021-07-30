22fc70eef29b8cd101b7fb7523fa3cdcf9cd1075
cactus-seok <sangpil95@gmail.com>
2021-07-30 23:33:22 +0900

    fix: User 엔티티에 Enum타입을 위한 클래스 생성

---
+++ src/main/java/com/webtooni/webtooniverse/domain/user/domain/UserGrade.java
@@ -0,0 +1,10 @@
+package com.webtooni.webtooniverse.domain.user.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserGrade {
+
+}
