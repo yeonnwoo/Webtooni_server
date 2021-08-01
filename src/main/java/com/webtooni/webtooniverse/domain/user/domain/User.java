78157ad1334f59ea3da347d47e3440122381784f
shoeone96 <dlwnddnjs96@gmail.com>
2021-08-02 06:11:32 +0900

    카카오 로그인 초안

--- src/main/java/com/webtooni/webtooniverse/domain/user/domain/User.java
+++ src/main/java/com/webtooni/webtooniverse/domain/user/domain/User.java
@@ -31,6 +31,8 @@ public class User {
     @Column(nullable = false)
     private String userGrade;
 
+    @Column(nullable = true)
+    private Long kakaoId;
 
     public User(String userName, String userEmail, String password, String userImg){
         this.userName = userName;
@@ -38,7 +40,16 @@ public class User {
         this.password = password;
         this.userImg = userImg;
         this.userGrade = "base";
+        this.kakaoId = null;
     }
+
+    public User(String password, Long kakaoId){
+        this.password = password;
+        this.userGrade = "base";
+        this.kakaoId = kakaoId;
+    }
+
+
 
 
 
