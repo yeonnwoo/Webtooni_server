11f97cab047cfae486e48f154dc0712539ed80fd
cactus-seok <sangpil95@gmail.com>
2021-08-04 02:23:38 +0900

    fix: QuerydslConfig 클래스 이름 변경

--- src/main/java/com/webtooni/webtooniverse/global/config/QuerydslConfig.java
+++ src/main/java/com/webtooni/webtooniverse/global/config/QuerydslConfig.java
@@ -8,7 +8,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 @Configuration
-public class QueryDSLConfig {
+public class QuerydslConfig {
     @PersistenceContext
     private EntityManager entityManager;
 
