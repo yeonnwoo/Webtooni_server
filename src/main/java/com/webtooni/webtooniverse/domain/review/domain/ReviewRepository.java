f2634c55644b827f0edc1b926ebd461fa951b37e
wonhee77 <leehi402@naver.com>
2021-08-03 15:22:57 +0900

    fix : user, review repository data jpa 상속

--- src/main/java/com/webtooni/webtooniverse/domain/review/domain/ReviewRepository.java
+++ src/main/java/com/webtooni/webtooniverse/domain/review/domain/ReviewRepository.java
@@ -1,4 +1,9 @@
 package com.webtooni.webtooniverse.domain.review.domain;
 
-public interface ReviewRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
 }
--- src/main/java/com/webtooni/webtooniverse/domain/user/domain/UserRepository.java
+++ src/main/java/com/webtooni/webtooniverse/domain/user/domain/UserRepository.java
@@ -1,4 +1,8 @@
 package com.webtooni.webtooniverse.domain.user.domain;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
