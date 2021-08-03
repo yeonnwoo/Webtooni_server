9b3a5f939edfd122d9ac8d795196be3d2f790acc
wonhee77 <leehi402@naver.com>
2021-08-03 15:04:08 +0900

    feat : timeStamped 설정

---
+++ src/main/java/com/webtooni/webtooniverse/global/utils/TimeStamped.java
@@ -0,0 +1,21 @@
+package com.webtooni.webtooniverse.global.utils;
+
+import lombok.Setter;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class TimeStamped {
+
+    @CreatedDate // 최초 생성 시점
+    private LocalDateTime createDate;
+}
