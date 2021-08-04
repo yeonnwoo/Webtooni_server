166a9aecd7185961790296c7ed170380679f87d0
cactus-seok <sangpil95@gmail.com>
2021-08-04 17:57:01 +0900

    feat: 베스트리뷰어를 가져오기 위한 dto생성

--- src/main/java/com/webtooni/webtooniverse/domain/user/controller/UserController.java
+++ src/main/java/com/webtooni/webtooniverse/domain/user/controller/UserController.java
@@ -1,4 +1,19 @@
 package com.webtooni.webtooniverse.domain.user.controller;
 
+import com.webtooni.webtooniverse.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
 public class UserController {
+
+    private final UserService userService;
+
+    //베스트 리뷰어(등급높은순)
+    @GetMapping("/api/v1/rank/reviewers")
+    public List<>
 }
--- src/main/java/com/webtooni/webtooniverse/domain/user/domain/UserGrade.java
+++ src/main/java/com/webtooni/webtooniverse/domain/user/domain/UserGrade.java
@@ -6,5 +6,5 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum UserGrade {
-
+    USER1, USER2, USER3
 }
