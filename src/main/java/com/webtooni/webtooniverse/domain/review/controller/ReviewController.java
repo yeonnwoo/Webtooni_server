be3477b81b62df50493c1ff062ab1ee943f3c7b3
cactus-seok <sangpil95@gmail.com>
2021-08-04 23:38:08 +0900

    feat: ReviewController 구현(임시)

--- src/main/java/com/webtooni/webtooniverse/domain/review/controller/ReviewController.java
+++ src/main/java/com/webtooni/webtooniverse/domain/review/controller/ReviewController.java
@@ -1,4 +1,22 @@
 package com.webtooni.webtooniverse.domain.review.controller;
 
+import com.webtooni.webtooniverse.domain.review.dto.response.ReviewMainResponseDto;
+import com.webtooni.webtooniverse.domain.review.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    //메인페이지에 리뷰(최신순/ 베스트순) 불러오기
+    @GetMapping("/api/v1/rank/reviews")
+    public List<ReviewMainResponseDto> getTotalReviews(){
+        return reviewService.getTotalReview();
+    }
 }
