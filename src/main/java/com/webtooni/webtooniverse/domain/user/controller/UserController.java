ca5e23f238b438cf3ff07281849c1d247a34dc41
shoeone96 <dlwnddnjs96@gmail.com>
2021-08-06 09:23:20 +0900

    usercontroller 주석 삭제

--- src/main/java/com/webtooni/webtooniverse/domain/user/controller/UserController.java
+++ src/main/java/com/webtooni/webtooniverse/domain/user/controller/UserController.java
@@ -9,10 +9,8 @@ import com.webtooni.webtooniverse.domain.user.security.UserDetailsImpl;
 import com.webtooni.webtooniverse.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RequiredArgsConstructor
