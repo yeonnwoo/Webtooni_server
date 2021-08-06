1d7775cd3e3eb55f701d2c434879f317d0e5c445
shoeone96 <dlwnddnjs96@gmail.com>
2021-08-06 09:35:37 +0900

    user 정리

--- src/main/java/com/webtooni/webtooniverse/domain/webtoon/domain/Webtoon.java
+++ src/main/java/com/webtooni/webtooniverse/domain/webtoon/domain/Webtoon.java
@@ -11,39 +11,28 @@ public class Webtoon {
     @Column(name = "toon_id")
     private Long id;
 
-    @Column(name = "toon_author")
-    private String toonAuthor;
-
-    @Column(name = "toon_title")
     private String toonTitle;
 
-    @Column(name = "toon_content")
+    private String toonAuthor;
+
     private String toonContent;
 
-    @Column(name = "toon_img")
     private String toonImg;
 
-    @Column(name = "toon_avg_point")
-    private Float toonAvgPoint;
+    private String toonWeekday;
 
-    @Column(name = "reql_url")
     private String realUrl;
 
-    @Column(name = "toon_age")
     private String toonAge;
 
-    @Column(name = "toon_flatform")
-    private String toonFlatform;
+    private String toonPlatform;
 
-    @Column(name = "review_count")
-    private Long reviewCount;
-    // 기본 값을 0으로 가져가야 하지 않을까
+    private float toonAvgPoint;
 
-    @Column(name = "finished")
-    private String finished;
-
-    @Column(name = "total_point_count")
     private int totalPointCount;
 
+    private int reviewCount;
+
+    private boolean finished;
 
 }
