0ae80f9a0abc6b9bdb81861547ddf87d4090cf90
cactus-seok <sangpil95@gmail.com>
2021-08-13 16:06:22 +0900

    fix : ReviewBestResponseDto,ReviewNewResponseDto 들어갈 변수 수정

--- src/main/java/com/webtooni/webtooniverse/domain/review/dto/response/ReviewBestResponseDto.java
+++ src/main/java/com/webtooni/webtooniverse/domain/review/dto/response/ReviewBestResponseDto.java
@@ -8,6 +8,8 @@ import com.webtooni.webtooniverse.domain.webtoon.domain.Webtoon;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class ReviewBestResponseDto {
@@ -17,32 +19,18 @@ public class ReviewBestResponseDto {
     private String userName;
     private float userPointNumber;
     private String reviewContent;
-    private Long toonId;
-    private String toonTitle;
-    private UserGrade userGrade;
-    private String toonImg;
-    private String toonAuthor;
-    private String toonPlatform;
-    private String toonWeekday;
-    private boolean finished;
+    private int likeCount;
+    private LocalDateTime creatDate;
 
 
-
-    public ReviewBestResponseDto(Long userId, int userImg, String userName, float userPointNumber, String reviewContent,
-                                 Long toonId, String toonTitle, UserGrade userGrade, String toonImg, String toonAuthor,
-                                 String toonPlatform, String toonWeekday, boolean finished) {
+    public ReviewBestResponseDto(Long userId, int userImg, String userName, float userPointNumber,
+                                String reviewContent, int likeCount, LocalDateTime creatDate) {
         this.userId = userId;
         this.userImg = userImg;
         this.userName = userName;
         this.userPointNumber = userPointNumber;
         this.reviewContent = reviewContent;
-        this.toonId = toonId;
-        this.toonTitle = toonTitle;
-        this.userGrade = userGrade;
-        this.toonImg = toonImg;
-        this.toonAuthor = toonAuthor;
-        this.toonPlatform = toonPlatform;
-        this.toonWeekday = toonWeekday;
-        this.finished = finished;
+        this.likeCount = likeCount;
+        this.creatDate = creatDate;
     }
 }
--- src/main/java/com/webtooni/webtooniverse/domain/review/dto/response/ReviewNewResponseDto.java
+++ src/main/java/com/webtooni/webtooniverse/domain/review/dto/response/ReviewNewResponseDto.java
@@ -15,33 +15,18 @@ public class ReviewNewResponseDto {
     private String userName;
     private float userPointNumber;
     private String reviewContent;
-    private Long toonId;
-    private String toonTitle;
-    private UserGrade userGrade;
-    private String toonImg;
-    private String toonAuthor;
-    private String toonPlatform;
-    private String toonWeekday;
-    private boolean finished;
+    private int likeCount;
     private LocalDateTime creatDate;
 
 
-    public ReviewNewResponseDto(Long userId, int userImg, String userName, float userPointNumber, String reviewContent,
-                                Long toonId, String toonTitle, UserGrade userGrade, String toonImg, String toonAuthor,
-                                String toonPlatform, String toonWeekday, boolean finished, LocalDateTime creatDate) {
+    public ReviewNewResponseDto(Long userId, int userImg, String userName, float userPointNumber,
+                                String reviewContent, int likeCount, LocalDateTime creatDate) {
         this.userId = userId;
         this.userImg = userImg;
         this.userName = userName;
         this.userPointNumber = userPointNumber;
         this.reviewContent = reviewContent;
-        this.toonId = toonId;
-        this.toonTitle = toonTitle;
-        this.userGrade = userGrade;
-        this.toonImg = toonImg;
-        this.toonAuthor = toonAuthor;
-        this.toonPlatform = toonPlatform;
-        this.toonWeekday = toonWeekday;
-        this.finished = finished;
+        this.likeCount = likeCount;
         this.creatDate = creatDate;
     }
 }
