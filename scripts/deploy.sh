84b7aaefbdd9d2278ee1370a3dad762157be6991
YeonwooKim <50096655+yeonnwoo@users.noreply.github.com>
2021-08-17 19:24:24 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -29,7 +29,8 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
+ 
 nohup java -jar \
- -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ubuntu/app/application-real-db.properties \
- -Dspring.profiles.active=real \
- $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ubuntu/app/application-oauth.properties,/home/ubuntu/app/application-real-db.properties \
+    -Dspring.profiles.active=real \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
