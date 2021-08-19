ded2a5b85e2ac5f4e2afd86c0b3889e8fb47dd87
wonhee77 <leehi402@naver.com>
2021-08-19 22:40:45 +0900

    fix: redis 설정 원격 서버로 변경

--- src/main/java/com/webtooni/webtooniverse/global/config/RedisConfig.java
+++ src/main/java/com/webtooni/webtooniverse/global/config/RedisConfig.java
@@ -1,31 +1,45 @@
 package com.webtooni.webtooniverse.global.config;
 
+import com.webtooni.webtooniverse.domain.webtoon.dto.response.BestReviewerWebtoonResponseDto;
 import java.time.Duration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHostName;
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
 
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-
-//        return new LettuceConnectionFactory("localhost", 6379);
-
         LettuceClientConfiguration lettuceClientConfiguration = LettuceClientConfiguration.builder()
             .commandTimeout(Duration.ofMinutes(1))
             .shutdownTimeout(Duration.ZERO)
             .build();
 
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(
-            "localhost", 6379);
-//        redisStandaloneConfiguration.setPassword(password);
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisHostName);
+        redisStandaloneConfiguration.setPort(redisPort);
+        redisStandaloneConfiguration.setPassword(redisPassword);
+
+
         return new LettuceConnectionFactory(redisStandaloneConfiguration,
             lettuceClientConfiguration);
     }
@@ -36,6 +50,15 @@ class RedisConfig {
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         redisTemplate.setDefaultSerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> objectRedisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer(BestReviewerWebtoonResponseDto.class));
         return redisTemplate;
     }
 }
