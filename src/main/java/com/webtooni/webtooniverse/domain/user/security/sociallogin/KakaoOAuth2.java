03b13ebbb22d4972317a1ad67105e2a3e96707e2
Yeonwoo-Kim <yeonkim0726@gmail.com>
2021-08-20 03:26:54 +0900

    fix : naver login

--- src/main/java/com/webtooni/webtooniverse/domain/user/security/sociallogin/KakaoOAuth2.java
+++ src/main/java/com/webtooni/webtooniverse/domain/user/security/sociallogin/KakaoOAuth2.java
@@ -35,7 +35,7 @@ public class KakaoOAuth2 {
         params.add("grant_type", "authorization_code");
         params.add("client_id", "9bf8aff1cb1460ec63268cd09c603a1a");
 //        params.add("redirect_uri", "http://webtooniverse-host.s3-website.ap-northeast-2.amazonaws.com/user/kakao");
-        params.add("redirect_uri", "http://localhost:3000/api/v1/user/kakao");
+        params.add("redirect_uri", "http://localhost:3000/api/v1/user/kakao/callback");
         params.add("code", authorizedCode);
 
         // HttpHeader와 HttpBody를 하나의 오브젝트에 담기
