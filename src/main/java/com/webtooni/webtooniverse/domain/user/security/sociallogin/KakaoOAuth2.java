0e53dd561b2a041d78cf74adf45e25513d389cca
YeonwooKim <50096655+yeonnwoo@users.noreply.github.com>
2021-08-24 12:06:24 +0900

    Update KakaoOAuth2.java

--- src/main/java/com/webtooni/webtooniverse/domain/user/security/sociallogin/KakaoOAuth2.java
+++ src/main/java/com/webtooni/webtooniverse/domain/user/security/sociallogin/KakaoOAuth2.java
@@ -34,7 +34,8 @@ public class KakaoOAuth2 {
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", "authorization_code");
         params.add("client_id", "9bf8aff1cb1460ec63268cd09c603a1a");
-        params.add("redirect_uri", "http://webtooniverse-host.s3-website.ap-northeast-2.amazonaws.com/user/kakao");
+        params.add("redirect_uri", "http://webtooni.co.kr/user/kakao");
+//        params.add("redirect_uri", "http://webtooniverse-host.s3-website.ap-northeast-2.amazonaws.com/user/kakao");
 //        params.add("redirect_uri", "http://localhost:8080/api/v1/user/kakao/callback");
 //        params.add("redirect_uri", "http://localhost:3000/user/kakao");
         params.add("code", authorizedCode);
