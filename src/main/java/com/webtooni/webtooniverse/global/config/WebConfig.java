e21f52568ae33f7869e2f821817618f8cd2fbd8e
YeonwooKim <50096655+yeonnwoo@users.noreply.github.com>
2021-08-24 11:46:50 +0900

    Update WebConfig.java

--- src/main/java/com/webtooni/webtooniverse/global/config/WebConfig.java
+++ src/main/java/com/webtooni/webtooniverse/global/config/WebConfig.java
@@ -12,8 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("http://localhost:3000",
-                "http://webtooniverse-host.s3-website.ap-northeast-2.amazonaws.com/","http://34.64.100.68")
-            .allowedOrigins("http://www.webtooni.co.kr/")
+                "http://webtooniverse-host.s3-website.ap-northeast-2.amazonaws.com/","http://34.64.100.68","http://webtooni.co.kr/")
             .allowedMethods(
                 HttpMethod.GET.name(),
                 HttpMethod.HEAD.name(),
