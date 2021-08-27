4848baf1b73f71f41a1b9691e78b29052495061f
wonhee77 <leehi402@naver.com>
2021-08-27 17:05:54 +0900

    fIx: 네이버 카카오 웹툰 랭킹 최소 리뷰 개수 2개 설정

--- src/main/java/com/webtooni/webtooniverse/domain/webtoon/domain/WebtoonRepositoryImpl.java
+++ src/main/java/com/webtooni/webtooniverse/domain/webtoon/domain/WebtoonRepositoryImpl.java
@@ -91,17 +91,18 @@ public class WebtoonRepositoryImpl implements WebtoonRepositoryCustom {
     //네이버 웹툰 Top10
     public List<Webtoon> getNaverRank() {
         return queryFactory.selectFrom(webtoon)
-            .where(webtoon.toonPlatform.eq("네이버"))
+            .where(webtoon.toonPlatform.eq("네이버").and(webtoon.reviewCount.goe(2)))
             .orderBy(webtoon.toonAvgPoint.desc())
             .limit(10)
             .fetch();
     }
 
+
     //카카오 웹툰 Top10
     public List<Webtoon> getKakaoRank() {
         return queryFactory.selectFrom(webtoon)
             .orderBy(webtoon.toonAvgPoint.desc())
-            .where(webtoon.toonPlatform.eq("카카오"))
+            .where(webtoon.toonPlatform.eq("카카오").and(webtoon.reviewCount.goe(2)))
             .limit(10)
             .fetch();
     }
