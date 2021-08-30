b6850a7ceda9182928aadc2431e951128415bfe1
wonhee77 <leehi402@naver.com>
2021-08-30 11:37:16 +0900

    feat: 유저 정보 responeDto에 userScore 추가

--- src/main/java/com/webtooni/webtooniverse/domain/user/dto/response/UserInfoOnlyResponseDto.java
+++ src/main/java/com/webtooni/webtooniverse/domain/user/dto/response/UserInfoOnlyResponseDto.java
@@ -1,7 +1,6 @@
 package com.webtooni.webtooniverse.domain.user.dto.response;
 
 import com.webtooni.webtooniverse.domain.user.domain.User;
-import com.webtooni.webtooniverse.domain.user.domain.UserGrade;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,11 +11,12 @@ public class UserInfoOnlyResponseDto {
     private String userName;
     private int userImg;
     private int userGrade;
+    private int userScore;
 
     public UserInfoOnlyResponseDto(User user) {
         this.userName = user.getUserName();
         this.userImg = user.getUserImg();
         this.userGrade = user.getUserGrade();
-
+        this.userScore = user.getUserScore();
     }
 }
