c09b0320a7ad1c7387a8e84bf6ce465899294563
YeonwooKim <50096655+yeonnwoo@users.noreply.github.com>
2021-09-29 20:07:15 +0900

    test: MyListRepository Impl test code 추가

--- src/test/java/com/webtooni/webtooniverse/domain/redis/RedisTest.java
+++ src/test/java/com/webtooni/webtooniverse/domain/redis/RedisTest.java
@@ -1,38 +1,38 @@
-// package com.webtooni.webtooniverse.domain.redis;
+package com.webtooni.webtooniverse.domain.redis;
 
-// import org.assertj.core.api.Assertions;
-// import org.junit.jupiter.api.Test;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.data.redis.core.RedisTemplate;
-// import org.springframework.data.redis.core.ValueOperations;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
-// import javax.transaction.Transactional;
-// import org.springframework.test.context.TestPropertySource;
+import javax.transaction.Transactional;
+import org.springframework.test.context.TestPropertySource;
 
-// import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
-// @SpringBootTest
-// @Transactional
-// public class RedisTest {
+@SpringBootTest
+@Transactional
+public class RedisTest {
 
-//     @Autowired
-//     RedisTemplate redisTemplate;
+    @Autowired
+    RedisTemplate redisTemplate;
 
-//     @Test
-//     void redisConnectionTest() {
-//         final String key = "a";
-//         final String data = "1";
+    @Test
+    void redisConnectionTest() {
+        final String key = "a";
+        final String data = "1";
 
-//         final ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
-//         valueOperations.set(key, data);
+        final ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(key, data);
 
-//         final String result = valueOperations.get(key);
-//         assertThat(result).isEqualTo(data);
+        final String result = valueOperations.get(key);
+        assertThat(result).isEqualTo(data);
 
-//         redisTemplate.delete("a");
-//         final String deleteResult = valueOperations.get(key);
-//         assertThat(deleteResult).isNull();
+        redisTemplate.delete("a");
+        final String deleteResult = valueOperations.get(key);
+        assertThat(deleteResult).isNull();
 
-//     }
-// }
+    }
+}
